0ac9b95d8a09d9058ef25dbd801e1277972c5bc8
Fredrik.N <fredrik.nelin@outlook.com>
2026-03-07 05:11:06 +0100

    Updated card layout for desktop resolution (#51)

--- components/feature/reviewgrid.tsx
+++ components/feature/reviewgrid.tsx
@@ -3,11 +3,13 @@ export default function ReviewGrid({ children }: { children: any }) {
 
     return <section className="my-8 mx-4">
         <ul className="
+                    md:w-9/10
+                    md:max-w-210
+                    md:mx-auto
                     grid
                     grid-cols-2
                     sm:grid-cols-1
                     justify-items-center">
-
             {children}
         </ul>
     </section>
